ba45fb45637cb5410864aa53a80f5885acfb254b
Mihkel Tiganik <mtiganik@gmail.com>
2023-03-21 11:13:53 +0200

    Found out TextBox can be modified in UI

--- Playtech/WpfApp1/MainWindow.xaml.cs
+++ Playtech/WpfApp1/MainWindow.xaml.cs
@@ -24,15 +24,45 @@ namespace WpfApp1
         {
             InitializeComponent();
 
-            //TextBox txtbox = new TextBox();
-            //txtbox.Text = "textbox1";
-            //this.stackpanel.Children.Add(txtbox);
+            ColumnDefinition gridCol1 = new ColumnDefinition();
+            ColumnDefinition gridCol2 = new ColumnDefinition();
+            ColumnDefinition gridCol3 = new ColumnDefinition();
 
-            //ComboBox cmb = new ComboBox();
-            //cmb.Items.Add("item 1");
-            //cmb.Items.Add("item 2");
-            //this.stackpanel.Children.Add(cmb);
+            this.dynamicGrid.ColumnDefinitions.Add(gridCol1);
+            this.dynamicGrid.ColumnDefinitions.Add(gridCol2);
+            this.dynamicGrid.ColumnDefinitions.Add(gridCol3);
+
+            RowDefinition gridRow1 = new RowDefinition();
+            gridRow1.Height = new GridLength(45);
+            RowDefinition gridRow2 = new RowDefinition();
+            gridRow2.Height = new GridLength(45);
+            RowDefinition gridRow3 = new RowDefinition();
+            gridRow3.Height = new GridLength(45);
+            this.dynamicGrid.RowDefinitions.Add(gridRow1);
+            this.dynamicGrid.RowDefinitions.Add(gridRow2);
+            this.dynamicGrid.RowDefinitions.Add(gridRow3);
+
+            TextBox txtBlock1 = BlackTextBox(1);
+            Grid.SetRow(txtBlock1, 0);
+            Grid.SetColumn(txtBlock1, 0);
+
+            this.dynamicGrid.Children.Add(txtBlock1);
 
         }
+
+        private static TextBox BlackTextBox(int Number)
+        {
+
+            TextBox txtBlock1 = new TextBox();
+            txtBlock1.Text = Number.ToString();
+            txtBlock1.FontSize = 16;
+            txtBlock1.FontWeight = FontWeights.Bold;
+            txtBlock1.Foreground = new SolidColorBrush(Colors.White);
+            txtBlock1.VerticalAlignment = VerticalAlignment.Center;
+            txtBlock1.HorizontalAlignment = HorizontalAlignment.Center;
+            return txtBlock1;
+        }
+
+
     }
 }
