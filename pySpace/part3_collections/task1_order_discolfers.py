44966e3e426203f0b123f31bd4dbb7fbdb142f1b
Mihkel Tiganik <mtiganik@gmail.com>
2023-05-04 16:43:55 +0300

    working solution

--- pySpace/part3_collections/task1_order_discolfers.py
+++ pySpace/part3_collections/task1_order_discolfers.py
@@ -1,31 +1,38 @@
+# import numpy as np
+
 def order_discgolfers(scores):
     players = {}
     for x in scores:
         for key,value in x.items():
-            if key in players: currList = players[key]
-            else: currList = ""
-            players[key] = currList + str(value)
-    for key in players: players[key] = players[key] [::-1] 
-    players = dict(sorted(players.items(), key=lambda item: item[1]))
+            players[key] = str(value) + players.get(key, "") 
+    players = dict(sorted(players.items()))
+    players = dict(sorted(players.items(), key=sortOrder))
     return list(players.keys())
 
+def sortOrder(item):
+    return item[1]
 
-scores = [{
-    'bob': 3,
-    'patric': 2,
-    'squidward': 4,
-}, {
-    'bob': 3,
-    'patric': 3,
-    'squidward': 3,
-}, {
-    'bob': 3,
-    'patric': 3,
-    'squidward': 2,
-}, {
-    'bob': 3,
-    'patric': 3,
-    'squidward': 3,
-}]
+# scores = [{
+#     'AA': 4,
+#     'CC': 1,
+#     'DD': 3,
+#     'BB': 3,
+# }, {
+#     'AA': 3,
+#     'CC': 3,
+#     'DD': 3,
+#     'BB': 3,
+    
+# }, {
+#     'AA': 3,
+#     'CC': 3,
+#     'DD': 3,
+#     'BB': 3,
+# }, {
+#     'AA': 2,
+#     'CC': 2,
+#     'DD': 2,
+#     'BB': 2,
+# }]
 
-print(order_discgolfers(scores))
+# print(order_discgolfers(scores))
