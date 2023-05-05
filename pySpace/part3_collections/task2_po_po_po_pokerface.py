9491d2918fb6fa595dab9280959c5b603c343ba9
Mihkel Tiganik <mtiganik@gmail.com>
2023-05-05 17:21:56 +0300

    working with bugs

--- pySpace/part3_collections/task2_po_po_po_pokerface.py
+++ pySpace/part3_collections/task2_po_po_po_pokerface.py
@@ -1,94 +1,98 @@
-from operator import itemgetter
+def getDeck(cards):
+    deck = ['0'*13]*4
+    for c in cards:
+        suit = c['suit']
+        if suit == 'hearts': i = 0
+        elif suit == 'diamonds': i=1
+        elif suit == 'clubs': i=2
+        elif suit == 'spades': i=3
+        deck[i] = deck[i][:c['value']-1] + '1' + deck[i][c['value'] :]
+    return deck
+
+
+def NoOfKind(deck,n):
+    for i in range(13):
+        currCnt = 0
+        for j in range(4):
+            if deck[j][i] == '1': currCnt += 1
+        if currCnt >= n: return True
+    return False
+
+def NoOfKindExclude(deck,n,exclude):
+    for i in range(13):
+        if i != exclude:
+            currCnt = 0
+            for j in range(4):
+                if deck[j][i] == '1': currCnt += 1
+            if currCnt >= n: return i
+    return -1
+
+def TwoPair(deck):
+    two = NoOfKindExclude(deck,2,-1)
+    if two == -1: return False
+    if NoOfKindExclude(deck,2,two) != -1: return True
+    return False
+
+
+def FullHouse(deck):
+    three = NoOfKindExclude(deck,3,-1)
+    if three == -1: return False
+    if NoOfKindExclude(deck,2,three) != -1: return True
+    return False
+
+def Straigh(deck):
+    s = '0'*13
+    for i in range(0,13):
+        if '1' in [deck[0][i], deck[1][i], deck[2][i], deck[3][i]]:
+            s = s[:i] + '1' + s[i + 1:]
+    if '11111' in s or (s[0] == '1' and s[-4:] == '1111'): return True
+    return False
+
+def Flush(deck):
+    for x in deck:
+        if x.count('1') >= 5: return True
+    return False
+
+def straightFlushCheck(deck):
+    for x in deck:
+        if "11111" in x or (x[0] == '1' and x[-4:]=='1111'):
+            return True
+    return False
+
+
+
 def pokervalue(cards):
-    if straightFlush(cards):
+    deck = getDeck(cards)
+    if straightFlushCheck(deck):
         return 'straight flush'
-    if fourKind(cards):
+    if NoOfKind(deck,4):
         return 'four of a kind'
-    if fullHouse(cards):
+    if FullHouse(deck):
         return 'full house'
-    if flush(cards):
+    if Flush(deck):
         return 'flush'
-    if straight(cards):
+    if Straigh(deck):
         return 'straight'
-    if threeOfKind(cards):
+    if NoOfKind(deck,3):
         return 'three of a kind'
-    if twoPair(cards):
+    if TwoPair(deck):
         return 'two pair'
-    if onePair(cards):
+    if NoOfKind(deck,2):
         return 'one pair'
     return 'high card'
 
-def straightFlush(cards):
-    return False
-def fourKind(cards): return False
-def fullHouse(cards): return False
-def flush(cards): return False
 
 
-def threeOfKind(cards): return False
-def twoPair(cards): return False
-def onePair(cards): return False
 
 cards = [
-    {'suit': 'hearts', 'value': 2},
-    {'suit': 'hearts', 'value': 1},
-    {'suit': 'hearts', 'value': 12},
-    {'suit': 'hearts', 'value': 9},
-    {'suit': 'hearts', 'value': 6},
-    {'suit': 'diamonds', 'value': 1},
-    {'suit': 'diamonds', 'value': 2},
-    {'suit': 'diamonds', 'value': 3},
     {'suit': 'clubs', 'value': 13},
+    {'suit': 'hearts', 'value': 13},
+    {'suit': 'diamonds', 'value': 6},
+    {'suit': 'clubs', 'value': 6},
+    {'suit': 'spades', 'value': 13},
     {'suit': 'spades', 'value': 6},
-    {'suit': 'spades', 'value': 2},
     {'suit': 'spades', 'value': 3},
-    {'suit': 'spades', 'value': 4},
-    {'suit': 'spades', 'value': 5},
 ]
-sorting = [
-    {'suit':'hearts', 'toMove':0},
-    {'suit':'diamonds', 'toMove': 16},
-    {'suit':'clubs', 'toMove': 32},
-    {'suit':'spades', 'toMove':48}
-    ]
 
-def getDeck(cards):
-    currVal = 0
-    for sort in sorting:
-        for card in cards:
-            if card['suit']==sort['suit']:
-                currVal += 1 << (sort['toMove'] + card['value'])
-    return currVal
+print(pokervalue(cards))
 
-#suit: hearts, 1-Diamonds, 2-Clubs, 3-Spades
-def getOneTypeCards(deck, suit):
-    return (deck >> suit*16) & 65535
-
-def getOneTypeAsList(deck):
-    list = []
-    for i in range(0,4):
-        list.append((deck >> i*16) & 65535)
-    return list
-
-def flushCheck(deck):
-    list = getOneTypeAsList(deck)
-    for i in list:
-        if bin(i).count('1') == 5:
-            return True
-    return False
-deck = getDeck(cards)
-
-def straightFlushCheck(deck):
-    list = getOneTypeAsList(deck)
-    for i in list:
-        str = bin(i)
-        if "11111" in str or (str[-2:] == '10' and len(str) == 16 and 'b1111' in str):
-            return True
-    return False
-
-straightFlushCheck(deck)
-
-id = getDeck(cards)
-width = 17
-bin_num = format(id, '0{}_b'.format(width+3))
-print(bin_num)
