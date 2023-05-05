b9272a2f990c7d583ef8926fd34e9d63267e68d6
Mihkel Tiganik <mtiganik@gmail.com>
2023-05-05 15:11:03 +0300

    straight flush done, ready to refactor

--- pySpace/part3_collections/task2_po_po_po_pokerface.py
+++ pySpace/part3_collections/task2_po_po_po_pokerface.py
@@ -24,34 +24,71 @@ def fourKind(cards): return False
 def fullHouse(cards): return False
 def flush(cards): return False
 
-# 10 9 8 7 6
-def straight(cards): 
-    # Sort the cards by value
-    sorted_cards = sorted(cards, key=lambda x: x['value'])
-    # Check if the values are consecutive
-    for i in range(3):
-        straightCnt = 0
-        for i in range(5):
-            if sorted_cards[i]['value'] != sorted_cards[i+1]['value']-1:
-                # If two consecutive values are not consecutive, return False
-                return False
-        if straightCnt == 5: return True
-        
-    # If all values are consecutive, return True
-    return True
 
 def threeOfKind(cards): return False
 def twoPair(cards): return False
 def onePair(cards): return False
 
 cards = [
-    {'suit': 'diamonds', 'value': 9},
-    {'suit': 'clubs', 'value': 2},
-    {'suit': 'hearts', 'value': 9},
+    {'suit': 'hearts', 'value': 2},
+    {'suit': 'hearts', 'value': 1},
     {'suit': 'hearts', 'value': 12},
-    {'suit': 'spades', 'value': 9},
+    {'suit': 'hearts', 'value': 9},
+    {'suit': 'hearts', 'value': 6},
+    {'suit': 'diamonds', 'value': 1},
+    {'suit': 'diamonds', 'value': 2},
+    {'suit': 'diamonds', 'value': 3},
+    {'suit': 'clubs', 'value': 13},
     {'suit': 'spades', 'value': 6},
+    {'suit': 'spades', 'value': 2},
     {'suit': 'spades', 'value': 3},
+    {'suit': 'spades', 'value': 4},
+    {'suit': 'spades', 'value': 5},
 ]
+sorting = [
+    {'suit':'hearts', 'toMove':0},
+    {'suit':'diamonds', 'toMove': 16},
+    {'suit':'clubs', 'toMove': 32},
+    {'suit':'spades', 'toMove':48}
+    ]
 
-print(pokervalue(cards))
+def getDeck(cards):
+    currVal = 0
+    for sort in sorting:
+        for card in cards:
+            if card['suit']==sort['suit']:
+                currVal += 1 << (sort['toMove'] + card['value'])
+    return currVal
+
+#suit: hearts, 1-Diamonds, 2-Clubs, 3-Spades
+def getOneTypeCards(deck, suit):
+    return (deck >> suit*16) & 65535
+
+def getOneTypeAsList(deck):
+    list = []
+    for i in range(0,4):
+        list.append((deck >> i*16) & 65535)
+    return list
+
+def flushCheck(deck):
+    list = getOneTypeAsList(deck)
+    for i in list:
+        if bin(i).count('1') == 5:
+            return True
+    return False
+deck = getDeck(cards)
+
+def straightFlushCheck(deck):
+    list = getOneTypeAsList(deck)
+    for i in list:
+        str = bin(i)
+        if "11111" in str or (str[-2:] == '10' and len(str) == 16 and 'b1111' in str):
+            return True
+    return False
+
+straightFlushCheck(deck)
+
+id = getDeck(cards)
+width = 17
+bin_num = format(id, '0{}_b'.format(width+3))
+print(bin_num)
