ff86d89c2235dca0c5874cc911ebe319b4f3bb3c
Mihkel Tiganik <mtiganik@gmail.com>
2023-06-05 16:01:49 +0300

    working example

--- pySpace/part5_functions/task1_equation_system.py
+++ pySpace/part5_functions/task1_equation_system.py
@@ -12,29 +12,30 @@ def solve(*equations):
             coefitsent = -valToGetZero/valOnTopCol
             for j in range(k,len(grid[0])):
                 grid[i][j] = grid[i][j] + coefitsent*grid[k][j]
-    
-    #last
-    grid[-1][-1] = grid[-1][-1] / grid[-1][-2]
-    grid[-1][-2] = 1
 
-    # secondLast
-    sumOfOtherVars = grid[-1][-1]*grid[-2][-2]
-    grid[-2][-2] = 0
-    grid[-2][-1] = (grid[-2][-1] - sumOfOtherVars)/grid[-2][-3]
-    grid[-2][-3] = 1
+    for i in range(-1, -len(grid)-1,-1):
+        sumOfOthers = 0
+        for j in range(-1,i,-1):
+            sumOfOthers = sumOfOthers + grid[j][-1]*grid[i][j-1]
+            grid[i][j-1] = 0
+        grid[i][-1] = (grid[i][-1] - sumOfOthers)/grid[i][i-1]
+        grid[i][i-1] = 1
 
-    # thirdLast
-    sumOfOtherVars = grid[-1][-1]*grid[-3][-2] + grid[-2][-1]*grid[-3][-3]
-    grid[-3][-2]=0
-    grid[-3][-3]=0
-    grid[-3][-1]=(grid[-3][-1] - sumOfOtherVars)/grid[-3][-4]
-    grid[-3][-4]=1
-
-    
-    for x in grid:
-        print(x)
+    keys = []
+    for k in gridDic[0]:
+        
+        keys.append(list(k.keys())[0])
+    keys.pop()
+    resDict = {}
+    for i in range(0,len(grid)):
+        resDict[keys[i]] = custRound(grid[i][-1])
+    return resDict
     
 
+def custRound(num):
+    val = round(num,2)
+    if round(num) - val == 0: return round(num)
+    return val
 def makeListFromDict(dic):
     res = []
     curr = []
@@ -62,15 +63,13 @@ def getEqData(eq):
     dict.append({"=":float(endVal)})
     return dict
 
-
 # solve("a+b+2c-7d=18","-5a+5b+7c-3d=-54","9a-5b-3c+9d=44","3a-3b-2c-6d=47") #want to know answer
 # solve("2x+3y=6","4x+9y=15") #x=1,5 y=1
 # solve("2e+3f=1", "0e+f=-1") # variable is 0 in one eq 
 # solve("0.1k-0.7m=-1.1","0.5k+0.2m=1.8") #k=-4,m=1
 # solve("-4x+3y-2z=-7","-8x-3y+4z=-5","16x-3y-5z=19") #x=1,y=-1,z=0
-
-
-
-solve("2v+w+x+y+z=4","v+2w+x+y+z=5","v+w+2x+y+z=6","v+w+x+2y+z=7","v+w+x+y+2z=8")
-
+# solve("2v+w+x+y+z=4","v+2w+x+y+z=5","v+w+2x+y+z=6","v+w+x+2y+z=7","v+w+x+y+2z=8")
 # solve("2a+2b-c+d=4","4a+3b-c+2d=6","8a+5b-3c+4d=12","3a+3b-2c+2d=6")
+# solve("-4a-6b+8c+7d=-21","-9a-3b-4c+d=82","-9a-b+4c-10d=111","3a-b+2c+0d=-29")
+# solve("7a+8b+9c+2d=-69","-5a+b+c+9d=-38","-8a+6b-4c-8d=74","9a-4b-9c+7d=-135")
+ 
